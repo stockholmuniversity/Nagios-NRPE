7cecff2cdbde92c68a95f1d6b8de6eb872897467
Andreas Marschke <andreas.marschke@gmail.com>
2013-01-07 15:45:58 +0100

    lib/Nagios/NRPE/Client.pm: add documentation

--- lib/Nagios/NRPE/Client.pm
+++ lib/Nagios/NRPE/Client.pm
@@ -18,6 +18,8 @@ Nagios::NRPE::Client - A Nagios NRPE client
 
 =head1 DESCRIPTION
 
+This Perl Module implements Version 2 of the NRPE-Protocol. With this module you can execute 
+
 =cut
 
 package Nagios::NRPE::Client;
@@ -55,6 +57,40 @@ use constant {
   STATE_OK                => 0
 };
 
+=head1 new()
+
+=head2 example
+
+ my $client = Nagios::NRPE::Client->new( host => "localhost", check => 'check_cpu');
+
+Takes a hash of options:
+
+=head2 host => <hostname or IP>
+
+The hostname or IP on which the NRPE-Server is running
+
+=head2 port => <Portnumber>
+
+The port number at which the NRPE-Server is listening
+
+=head2 timeout => <number in seconds>
+
+Timeout for TCP/IP communication
+
+=head2 arglist => ["arg","uments"]
+
+List of arguments attached to the check
+
+=head2 check => "check_command"
+
+Command defined in the nrpe.cfg on the NRPE-Server
+
+=head2 ssl => 0,1
+
+Use or don't use SSL
+
+=cut
+
 sub new {
   my ($class,%hash) = @_;
   my $self = {};
@@ -68,6 +104,22 @@ sub new {
   bless $self,$class;
 
 }
+
+=head1 run()
+
+  my $response = $client->run();
+
+Runs the communication to the server and returns a hash of the form:
+
+  {
+    version => NRPE_VERSION,
+    type => RESPONSE_TYPE,
+    crc32 => CRC32_CHECKSUM,
+    code => RESPONSE_CODE,
+    buffer => CHECK_OUTPUT
+  }
+
+=cut
 
 sub run {
   my $self = shift;
