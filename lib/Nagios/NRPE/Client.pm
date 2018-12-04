13f4001c40db5120f9bdaf4b626cf9846f0023c7
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-12-04 10:52:58 +0000

    Linked up AUTHORS file.

--- lib/Nagios/NRPE/Client.pm
+++ lib/Nagios/NRPE/Client.pm
@@ -21,7 +21,7 @@ checks on a remote server.
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013-2018 by the authors (see AUTHORS file).
+This software is copyright (c) 2013-2018 by the authors (see L<AUTHORS|https://github.com/stockholmuniversity/Nagios-NRPE/blob/master/AUTHORS> file).
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
